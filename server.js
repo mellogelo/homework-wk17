338cebe5472adf57694c234165b7559e4202dd60
Ralph Magbanua <rmagbanua09@gmail.com>
2020-06-28 16:38:20 -0700

    finish api route

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
+
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +18,8 @@ mongoose.connect(process.env.MONGOD_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require(".routes/htmlRoutes.js"));
+app.use(require("./routes/api"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
